f501a0c31e736737568540a2e738c807e497cb03
AlpcanVelayi <alpcanv@gmail.com>
2021-03-12 00:23:21 +0300

    Add project files.

---
+++ _DictionaryYapisi/MyDictionary.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _DictionaryYapisi
+{
+    class MyDictionary<KeyType, ValueType>
+    {
+        KeyType[] anahtarTipi;
+        ValueType[] degerTipi;
+
+        public MyDictionary()
+        {
+            anahtarTipi = new KeyType[0];
+            degerTipi = new ValueType[0];
+        }
+        public void Add(KeyType item, ValueType item2)
+        {
+            KeyType[] tempAnahtar = anahtarTipi;
+            ValueType[] tempDeger = degerTipi;
+
+            anahtarTipi = new KeyType[anahtarTipi.Length + 1];
+            degerTipi = new ValueType[degerTipi.Length + 1];
+
+            for (int i = 0; i < tempAnahtar.Length; i++)
+            {
+                anahtarTipi[i] = tempAnahtar[i];
+            }
+            anahtarTipi[anahtarTipi.Length - 1] = item;
+
+            for (int i = 0; i < tempDeger.Length; i++)
+            {
+                degerTipi[i] = tempDeger[i];
+            }
+            degerTipi[anahtarTipi.Length - 1] = item2;
+
+        }
+        public void Listeleme()
+        {
+            for (int i = 0; i < anahtarTipi.Length; i++)
+            {
+                Console.WriteLine("Numaranız : " + anahtarTipi[i] + "\t" + "Girdiğiniz değer : " + degerTipi[i]);
+            }
+
+        }
+
+    }
+}
---
+++ _DictionaryYapisi/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+namespace _DictionaryYapisi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> _group = new MyDictionary<int, string>();
+            _group.Add(31, "Gözde Doğuer");
+            _group.Add(484, "Alpcan Velayi");
+            _group.Add(844, "Baran Velayi");
+            _group.Add(112, "Ceyshan Gülistanzade");
+            _group.Add(1, "Cimcim Velayi");
+
+            _group.Listeleme();
+            try
+            {
+                for (int i = 0; i < i + 1; i++)
+                {
+                    Console.WriteLine("\n");
+                    int _newNo;
+                    string _newName;
+                    Console.Write("Eklemek istediğiniz kişinin numarasını giriniz : ");
+                    _newNo = Convert.ToInt32(Console.ReadLine());
+
+                    Console.Write("Eklemek istediğiniz kişinin adını ve soyadını giriniz : ");
+                    _newName = Console.ReadLine();
+                    Console.Clear();
+                    _group.Add(_newNo, _newName);
+
+                    _group.Listeleme();
+
+                }
+            }
+            catch
+            {
+                Console.Clear();
+                Console.WriteLine("Değerlerden birini yanlış girdiniz\n");
+                for (int i = 0; i < i + 1; i++)
+                {
+                    int _newNo;
+                    string _newName;
+                    Console.Write("Eklemek istediğiniz kişinin numarasını giriniz : ");
+                    _newNo = Convert.ToInt32(Console.ReadLine());
+
+                    Console.Write("Eklemek istediğiniz kişinin adını ve soyadını giriniz : ");
+                    _newName = Console.ReadLine();
+                    Console.Clear();
+                    _group.Add(_newNo, _newName);
+
+                    _group.Listeleme();
+                    Console.WriteLine("\n");
+                }
+
+            }
+
+        }
+    }
+}
